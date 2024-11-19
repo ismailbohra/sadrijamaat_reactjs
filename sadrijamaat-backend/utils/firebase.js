fd139ed3aa3bdae47fec802d76d3bd637becaacd
ismailbohra <ismailbohra99@gmail.com>
2024-11-20 02:06:55 +0530

    update

--- sadrijamaat-backend/utils/firebase.js
+++ sadrijamaat-backend/utils/firebase.js
@@ -1,12 +1,10 @@
-// config/firebase.js
-const admin = require('firebase-admin');
-const path = require('path');
-require('dotenv').config();
+const admin = require("firebase-admin");
+require("dotenv").config();
 
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(require(path.resolve(serviceAccountPath))),
+  credential: admin.credential.cert(serviceAccount),
 });
 
-module.exports = {admin};
+module.exports = { admin };
